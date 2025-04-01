6712bf2017f70bc06e4320ded4cb9835f8f88695
Akshay Pai <akshaypaik@gmail.com>
2025-04-01 17:03:08 +0530

    sending email for contact us using emailjs

--- src/components/Body/Contact/Contact.jsx
+++ src/components/Body/Contact/Contact.jsx
@@ -1,14 +1,29 @@
 import React, { useRef } from 'react';
 import './Contact.css';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
 
     const name = useRef(null);
     const email = useRef(null);
     const message = useRef(null);
+    const form = useRef();
 
     const handleContactFormSubmission = (e) => {
         e.preventDefault();
+
+        emailjs.sendForm('service_edepv3v', 'template_h1pt6ke', form.current, {
+            publicKey: 'jKM3xD9KvUXSIomRR',
+        })
+            .then(
+                () => {
+                    console.log('SUCCESS! Email Sent Successfully');
+                },
+                (error) => {
+                    console.log('Email Sending FAILED...', error.text);
+                },
+            );
+
         resetForm();
     }
 
@@ -21,10 +36,10 @@ const Contact = () => {
     return (
         <div>
             <h1 className='section-title'>Contact</h1>
-            <form className='contact-form' onSubmit={(e) => handleContactFormSubmission(e)}>
-                <input type='text' placeholder='Name' required ref={name} />
-                <input type='email' placeholder='Email' required ref={email} />
-                <textarea type='text' placeholder='Message' required ref={message} />
+            <form ref={form} className='contact-form' onSubmit={(e) => handleContactFormSubmission(e)}>
+                <input type='text' name="user_name" placeholder='Name' required ref={name} />
+                <input type='email' name="user_email" placeholder='Email' required ref={email} />
+                <textarea name="message" placeholder='Message' required ref={message} />
                 <button>Send</button>
             </form>
         </div>
